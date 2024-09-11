fb54a9a2ee70f3545110200473d1945ca6eb9e02
Lo-Lap <116972772+Lo-Lap@users.noreply.github.com>
2024-09-11 19:09:54 +0300

    Add file for lab1

--- lab1/lab_interval_matrix.py
+++ lab1/lab_interval_matrix.py
@@ -21,14 +21,4 @@ for eps in eps_list:
         min_det = det
 
 print("min_eps=", min_eps)
-print(min_det)
-# inter = ip.Interval([1, 5])
-print()
-eps = 0.0
-print("eps=", eps)
-A_eps = [
-        [[1-eps, 1+eps], [0.9-eps, 0.9+eps]],
-        [[1-eps, 1+eps], [1.1-eps, 1.1+eps]]
-    ]
-# print(A_eps[1][1])
-print(ip.Interval(A_eps[0][0])*ip.Interval(A_eps[1][1]) - ip.Interval(A_eps[0][1])*ip.Interval(A_eps[1][0]))
+print("Определитель матрицы=", min_det)

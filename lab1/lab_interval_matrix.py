def222b25bdaa07bd3eca7c812f56e48e2464bae
Lo-Lap <116972772+Lo-Lap@users.noreply.github.com>
2024-09-18 17:54:29 +0300

    Update file for lab1

--- lab1/lab_interval_matrix.py
+++ lab1/lab_interval_matrix.py
@@ -9,22 +9,23 @@ def determinant_2(A):
 def intersection_int(A, B):
     return [max(min(A), min(B)), min(max(A), max(B))]
 
-# eps_list = [e/10000 for e in range(0, 10000)]
-# eps_suitable = []
-# det_in_0 = []
-# for eps in eps_list:
-#     A_eps = [
-#         [[1.05-eps, 1.05+eps], [0.95-eps, 0.95+eps]],
-#         [[1-eps, 1+eps], [1-eps, 1+eps]]
-#     ]
-#
-#     det = determinant_2(A_eps)
-#     if 0 in det:
-#         eps_suitable.append(eps)
-#         det_in_0.append(det)
-#
-# print("подходящие eps_min=", min(eps_suitable), " eps_max", max(eps_suitable))
-# print("соответсвующие определители det_min", det_in_0[0], " det_max", det_in_0[-1])
+
+eps_list = [e/10000 for e in range(0, 10000)]
+eps_suitable = []
+det_in_0 = []
+for eps in eps_list:
+    A_eps = [
+        [[1.05-eps, 1.05+eps], [0.95-eps, 0.95+eps]],
+        [[1-eps, 1+eps], [1-eps, 1+eps]]
+    ]
+
+    det = determinant_2(A_eps)
+    if 0 in det:
+        eps_suitable.append(eps)
+        det_in_0.append(det)
+
+print("подходящие eps_min=", min(eps_suitable), " eps_max", max(eps_suitable))
+print("соответсвующие определители det_min", det_in_0[0], " det_max", det_in_0[-1])
 
 print()
 eps = 0.0251

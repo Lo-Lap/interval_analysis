3a9d2837910dea005844ad7d06890c43d3207b70
Lo-Lap <116972772+Lo-Lap@users.noreply.github.com>
2025-01-16 21:28:27 +0300

    Update data preparation and draw function

--- lab3/data_preparation.py
+++ lab3/data_preparation.py
@@ -1,6 +1,7 @@
 import struct
 import numpy as np
 import intvalpy as ip
+import matplotlib.pyplot as plt
 
 
 def read_bin_file_with_numpy(file_path):
@@ -27,6 +28,36 @@ def scalar_to_interval(x, rad):
 scalar_to_interval_vec = np.vectorize(scalar_to_interval)
 
 
+def boxplot_T(data_list):
+    LQ = np.quantile(data_list, 0.25)
+    UQ = np.quantile(data_list, 0.75)
+    IQR = UQ - LQ
+    s = sorted(data_list)
+    x_L = max(s[0], LQ - 3 / 2 * IQR)
+    x_U = min(s[-1], UQ + 3 / 2 * IQR)
+
+    return np.array([x_L, x_U])
+
+
+def checking_for_anomaly(data_list, ranges):
+    idx = []
+    A = []
+    for i in range(len(data_list)):
+        row = [f"x({i + 1})", f"{data_list[i]}"]
+        if data_list[i] not in ip.Interval(ranges):
+            row.append("аномальна")
+            idx.append(i)
+            A.append(row)
+    return idx
+
+
+def remove_outliers(data_list):
+    ranges = boxplot_T(data_list)
+    idx = checking_for_anomaly(data_list, ranges)
+    data_list = np.delete(data_list, idx)
+    return data_list
+
+
 def get_avg(data):
     avg = [[0]*8]*1024
     for i in range(len(data)): # 100
@@ -38,22 +69,41 @@ def GetData():
     x_data = read_bin_file_with_numpy('-0.205_lvl_side_a_fast_data.bin')
     y_data = read_bin_file_with_numpy('0.225_lvl_side_a_fast_data.bin')
 
-    # x_avg = get_avg(x_data)
-    # y_avg = get_avg(y_data)
+    # x_data = get_avg(x_data)
+    # y_data = get_avg(y_data)
 
     x_voltage = x_data / 16384.0 - 0.5
     y_voltage = y_data / 16384.0 - 0.5
 
-    # x_avg = x_avg / 16384.0 - 0.5
-    # y_avg = y_avg / 16384.0 - 0.5
+    plt.figure(figsize=(12, 9))
+    plt.subplot(1, 2, 1)
+    plt.hist(x_voltage.flatten(), bins=100)
+    plt.title("X до обработки")
+    plt.subplot(1, 2, 2)
+    plt.hist(y_voltage.flatten(), bins=100)
+    plt.title("Y до обработки")
+    plt.savefig(f"XYbefore")
+    plt.show()
+
+    x_after = remove_outliers(x_voltage.flatten())
+    y_after = remove_outliers(y_voltage.flatten())
+
+    plt.figure(figsize=(12, 9))
+    plt.subplot(1, 2, 1)
+    plt.hist(x_after, bins=100)
+    plt.title("X после отбрасывания выбросов")
+    plt.subplot(1, 2, 2)
+    plt.hist(y_after, bins=100)
+    plt.title("Y после отбрасывания выбросов")
+    plt.savefig(f"XYafter")
+    plt.show()
 
     rad = 2 ** (-14)
 
-    X = scalar_to_interval_vec(x_voltage, rad).flatten()
-    Y = scalar_to_interval_vec(y_voltage, rad).flatten()
+    X = scalar_to_interval_vec(x_after, rad).flatten()
+    Y = scalar_to_interval_vec(y_after, rad).flatten()
 
-    # X_mode = scalar_to_interval_vec(x_avg, rad).flatten()
-    # Y_mode = scalar_to_interval_vec(y_avg, rad).flatten()
     print("Convert X and Y")
     return X, Y
+
 
--- lab3/lab3_main.py
+++ lab3/lab3_main.py
@@ -27,14 +27,14 @@ def mode(X):
     # if not (np.isnan(InterSec.a) and np.isnan(InterSec.b)):
     #     return InterSec
 
-    Y = []
+    Y_g = []
     for el in X:
-        Y.append(el.a)
-        Y.append(el.b)
+        Y_g.append(el.a)
+        Y_g.append(el.b)
 
-    Y.sort()
+    Y_g.sort()
 
-    Z = [ip.Interval(Y[i], Y[i + 1]) for i in range(len(Y) - 1)]
+    Z = [ip.Interval(Y_g[i], Y_g[i + 1]) for i in range(len(Y_g) - 1)]
 
     mu = [sum(1 for x_i in X if z_i in x_i) for z_i in Z]
 
@@ -56,19 +56,19 @@ def mode(X):
             current_interval = next_interval
 
     mode_.append(current_interval)
+    print(mode_)
+    return np.array(mode_)
 
-    return mode_
 
-
-def med_K(X):
-    c_inf = [ip.inf(el) for el in X]
-    c_sup = [ip.sup(el) for el in X]
+def med_K(X_data):
+    c_inf = [ip.inf(el) for el in X_data]
+    c_sup = [ip.sup(el) for el in X_data]
 
     return ip.Interval(np.median(c_inf), np.median(c_sup))
 
 
-def med_P(X):
-    x = sorted(X, key=cmp_to_key(lambda x, y: (x.a + x.b) / 2 - (y.a + y.b) / 2))
+def med_P(X_data):
+    x = sorted(X_data, key=cmp_to_key(lambda x, y: (x.a + x.b) / 2 - (y.a + y.b) / 2))
 
     index_med = len(x) // 2
 
@@ -84,20 +84,28 @@ def coefficient_Jakkard(X_data, Y_data=None):
         x_sup = [ip.sup(x) for x in X_data]
         return (min(x_sup) - max(x_inf)) / (max(x_sup) - min(x_inf))
 
-    if isinstance(X_data, ip.ClassicalArithmetic) and isinstance(Y_data, ip.ClassicalArithmetic):
-        return (min(ip.sup(X_data), ip.sup(Y_data)) - max(ip.inf(X_data), ip.inf(Y_data))) / \
-            (max(ip.sup(X_data), ip.sup(Y_data)) - min(ip.inf(X_data), ip.inf(Y_data)))
-
-    jakkard_v = []
-    for x, y in zip(X_data, Y_data):
-        coeff = (min(ip.sup(x), ip.sup(y)) - max(ip.inf(x), ip.inf(y))) / (max(ip.sup(x), ip.sup(y)) - min(ip.inf(x), ip.inf(y)))
-        jakkard_v.append(coeff)
-
-    return jakkard_v
+    # if isinstance(X_data, ip.ClassicalArithmetic) and isinstance(Y_data, ip.ClassicalArithmetic):
+    #     return (min(ip.sup(X_data), ip.sup(Y_data)) - max(ip.inf(X_data), ip.inf(Y_data))) / \
+    #         (max(ip.sup(X_data), ip.sup(Y_data)) - min(ip.inf(X_data), ip.inf(Y_data)))
+    #
+    # # jakkard_v = []
+    # # for x, y in zip(X_data, Y_data):
+    # #     coeff = (min(ip.sup(x), ip.sup(y)) - max(ip.inf(x), ip.inf(y))) / (
+    # #                 max(ip.sup(x), ip.sup(y)) - min(ip.inf(x), ip.inf(y)))
+    # #     jakkard_v.append(coeff)
+    #
+    # x_inf = [ip.inf(x) for x in X_data]
+    # x_sup = [ip.sup(x) for x in X_data]
+    # y_inf = [ip.inf(x) for x in Y_data]
+    # y_sup = [ip.sup(x) for x in Y_data]
+    # coeff = (min(max(x_sup), max(y_sup)) - max(min(x_inf), min(y_inf))) / \
+    #         (max(max(x_sup), max(y_sup)) - min(min(x_inf), min(y_inf)))
+    #
+    # return coeff
 
 
 def argmaxF(f, a, b, eps):
-    lmbd = a + (3 - 5 ** 0.5) * (b - a)/2
+    lmbd = a + (3 - 5 ** 0.5) * (b - a) / 2
     mu = b - (3 - 5 ** 0.5) * (b - a) / 2
     f_lambda = f(lmbd)
     f_mu = f(mu)
@@ -117,62 +125,105 @@ def argmaxF(f, a, b, eps):
                 break
             mu = lmbd
             f_mu = f_lambda
-            lmbd = a + (3 - 5 ** 0.5) * (b - a)/2
+            lmbd = a + (3 - 5 ** 0.5) * (b - a) / 2
             f_lambda = f(lmbd)
 
-        print(a)
-        print(b)
+        # print(a)
+        # print(b)
 
-    return (a+b) / 2
+    return (a + b) / 2
 
 
 def func_a(a):
-    return np.mean(coefficient_Jakkard(X + a, Y))
+    new_X = X + a
+    XY = np.concatenate((new_X, Y))
+    return coefficient_Jakkard(XY)
 
 
 def func_t(t):
-    return np.mean(coefficient_Jakkard(X * t, Y))
+    new_X = X*t
+    XY = np.concatenate((new_X, Y))
+    return coefficient_Jakkard(XY)
 
 
 def func_mode_a(a):
-    return np.mean(coefficient_Jakkard(mode(X + a), mode(Y)))
+    new_X = X + a
+    XY = np.concatenate((mode(new_X), Y_mode))
+    # XY = np.concatenate((X_mode+a, Y_mode))
+    return coefficient_Jakkard(XY)
 
 
 def func_mode_t(t):
-    return np.mean(coefficient_Jakkard(mode(X * t), mode(Y)))
+    new_X = X*t
+    XY = np.concatenate((mode(new_X), Y_mode))
+    # XY = np.concatenate((X_mode*t, Y_mode))
+    return coefficient_Jakkard(XY)
 
 
 def func_med_p_a(a):
-    return np.mean(coefficient_Jakkard(med_P(X + a), med_P(Y)))
+    new_X = X + a
+    XY = np.array([med_P(new_X), med_P(Y)])
+    return coefficient_Jakkard(XY)
 
 
 def func_med_p_t(t):
-    return np.mean(coefficient_Jakkard(med_P(X * t), med_P(Y)))
+    new_X = X * t
+    XY = np.array([med_P(new_X), med_P(Y)])
+    return coefficient_Jakkard(XY)
 
 
 def func_med_k_a(a):
-    return np.mean(coefficient_Jakkard(med_K(X + a), med_K(Y)))
+    new_X = X + a
+    XY = np.array([med_K(new_X), med_K(Y)])
+    return coefficient_Jakkard(XY)
 
 
 def func_med_k_t(t):
-    return np.mean(coefficient_Jakkard(med_K(X * t), med_K(Y)))
+    new_X = X * t
+    XY = np.array([med_K(new_X), med_K(Y)])
+    return coefficient_Jakkard(XY)
 
 
 def draw_func(f, a, b, parametr: str, func=""):
-    X_linsp = np.linspace(a, b, 100)
+    if parametr == "a":
+        # X_linsp = np.linspace(a, b, 175)
+        X_linsp = np.linspace(a, b, 50)
+    else:
+        # X_linsp = np.linspace(a, b, 300)
+        X_linsp = np.linspace(a, b, 175)
     y = [f(x) for x in X_linsp]
     y_max = max(y)
     y_min = min(y)
     ind_max = y.index(y_max)
+    ind_min = y.index(y_min)
     x_max = X_linsp[ind_max]
+    print(x_max, y_max)
 
+    from scipy.interpolate import interp1d
+    f1 = interp1d(np.array(y[0:ind_max + 1]), np.array(X_linsp[0:ind_max + 1]), kind='linear')
+    f2 = interp1d(np.array(y[ind_max:]), np.array(X_linsp[ind_max:]), kind='linear')
+
+    if y_max < 0:
+        int_line = (3 * y[ind_max] + y[ind_min]) / 4
+        ext_line = (y[ind_max] + 3 * y[ind_min]) / 4
+    else:
+        ext_line = 0.5
+        int_line = 0.8
+
+    # print(f"internal: {int_line}, x=[{f1(int_line)},{f2(int_line)}]")
+    # print(f"external: {ext_line}, x=[{f1(ext_line)},{f2(ext_line)}]")
+
+    plt.figure(figsize=(12, 9))
     plt.plot(X_linsp, y, color='b')
-    plt.xlabel(f"{parametr}")
+    plt.xlabel(f"{parametr}, {parametr}_max={round(x_max, 5)}")
     plt.ylabel(f"Ji({parametr}, {func}(X), {func}(Y))")
-    plt.axvline(x=x_max, linestyle='--', color='r')
-    plt.text(x_max+0.15, (y_max + y_min)/2, f"x = {round(x_max, 4)}", color='red',
-             ha='center')  # Positioned near the line
+    plt.axvline(x=x_max, linestyle='--', color='black')
 
+    # plt.hlines(int_line, f1(int_line), f2(int_line), linestyles='dashed', label='internal', colors='green')
+    # plt.hlines(ext_line, f1(ext_line), f2(ext_line), linestyles='dashed', label='external', colors='red')
+    # plt.legend()
+    # plt.text(x_max + 0.15, (y_max + y_min) / 2, f"{parametr} = {round(x_max, 4)}", color='red',
+    #          ha='center')  # Positioned near the line
     plt.title("Jaccard Index")
     plt.savefig(f"Jaccadrd-{parametr}-{func}")
     plt.show()
@@ -184,47 +235,54 @@ def draw_func_all(i, f, a, b, parametr: str, func=""):
     y = np.array([f(x) for x in X_linsp])
     plt.plot(X_linsp, y, color=colors[i], label=f"Ji({parametr}, {func}(X), {func}(Y))", alpha=0.7)
 
-    # plt.xlabel(f"{parametr}")
-    # plt.ylabel(f"Ji({parametr}, {func}(X), {func}(Y))")
-    # plt.title("Jaccard Index")
-    # plt.show()
-    # plt.savefig(f"Jaccadrd-{parametr}-{func}")
+    plt.xlabel(f"{parametr}")
+    plt.ylabel(f"Ji({parametr}, {func}(X), {func}(Y))")
+    plt.title("Jaccard Index")
+    plt.show()
+    plt.savefig(f"Jaccadrd-{parametr}-{func}")
 
 
 if __name__ == "__main__":
     X, Y = GetData()
-
-    # # Функционал = Ji(const, X, Y)
-    draw_func(func_a, 0, 1, "a")
+    # Функционал = Ji(const, X, Y)
+    draw_func(func_a, 0.32, 0.37, "a")
     # a_f = argmaxF(func_a, 0, 1, 1e-3)
     # print(a_f, func_a(a_f))
-    draw_func(func_t, -4, 0, "t")
+
+    draw_func(func_t, -1.06, -0.98, "t")
     # t_f = argmaxF(func_t, -4, 0, 1e-3)
     # print(t_f, func_t(t_f))
 
     # # Функционал = Ji(const,mode(X), mode(Y))
-    # draw_func(func_mode_a, 0, 1)
-    # # a_f_mode = argmaxF(func_mode_a, 0, 1, 1e-3)
-    # # print(a_f_mode, func_mode_a(a_f_mode))
-    # draw_func(func_mode_t, -4, 0)
-    # t_f_mode = argmaxF(func_mode_t, -4, 0, 1e-3)
+
+    # X_mode = []
+    # Y_mode = []
+
+    # X_mode = mode(X)
+    Y_mode = mode(Y)
+
+    draw_func(func_mode_a, 0.34692, 0.34693, "a", "mode")
+    # a_f_mode = argmaxF(func_mode_a, 0.3425, 0.3445, 1e-5)
+    # print(a_f_mode, func_mode_a(a_f_mode))
+    draw_func(func_mode_t, -1.0398, -1.0394, "t", "mode")
+    # t_f_mode = argmaxF(func_mode_t, -1.017, -1.011, 1e-5)
     # print(t_f_mode, func_mode_t(t_f_mode))
 
-    # # Функционал = Ji(const,med_K(X), med_K(Y))
-    draw_func(func_med_k_a, 0, 1, "a", "med_K")
-    # a_f_med_k = argmaxF(func_med_k_a, 0, 1, 1e-3)
-    # print(a_f_med_k, func_med_k_a(a_f_med_k))
-    draw_func(func_med_k_t, -4, 0, "t", "med_K")
-    # t_f_med_k = argmaxF(func_med_k_t, -4, 0, 1e-3)
-    # print(t_f_med_k, func_med_k_t(t_f_med_k))
+    # Функционал = Ji(const,med_K(X), med_K(Y))
+    draw_func(func_med_k_a, 0.3425, 0.3445, "a", "med_K")
+    # # a_f_med_k = argmaxF(func_med_k_a, 0, 1, 1e-3)
+    # # print(a_f_med_k, func_med_k_a(a_f_med_k))
+    draw_func(func_med_k_t, -1.017, -1.011, "t", "med_K")
+    # # t_f_med_k = argmaxF(func_med_k_t, -4, 0, 1e-3)
+    # # print(t_f_med_k, func_med_k_t(t_f_med_k))
 
-    # # Функционал = Ji(const,med_р(X), med_р(Y))
-    draw_func(func_med_p_a, 0, 1, "a", "med_p")
-    # a_f_med_p = argmaxF(func_med_p_a, 0, 1, 1e-3)
-    # print(a_f_med_p, func_med_p_a(a_f_med_p))
-    draw_func(func_med_p_t, -4, 0, "t", "med_p")
-    # t_f_med_p = argmaxF(func_med_p_t, -4, 0, 1e-3)
-    # print(t_f_med_p, func_med_p_t(t_f_med_p))
+    # Функционал = Ji(const,med_р(X), med_р(Y))
+    draw_func(func_med_p_a, 0.3425, 0.3445, "a", "med_p")
+    # # a_f_med_p = argmaxF(func_med_p_a, 0, 1, 1e-3)
+    # # print(a_f_med_p, func_med_p_a(a_f_med_p))
+    draw_func(func_med_p_t, -1.017, -1.011, "t", "med_p")
+    # # t_f_med_p = argmaxF(func_med_p_t, -4, 0, 1e-3)
+    # # print(t_f_med_p, func_med_p_t(t_f_med_p))
 
     # funcs = [func_a, func_t, func_a_med_k, func_t_med_k, func_a_med_p, func_t_med_p]
     # funcs_str = ["", "", "med_k", "med_k", "med_p", "med_p"]
